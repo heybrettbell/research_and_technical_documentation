8db87eb2963295b706f1de04589cb7d9727fa91d
heybrettbell <brett@grassrootsonline.ca>
2017-06-30 06:51:38 -0400

    String and Array research complete

---
+++ research.rb
@@ -0,0 +1,40 @@
+String
+
+length - returns the character count of string
+
+strip - returns the string with leading and/or trailing whitespace removed
+
+split - splits string into substrings with a delimiter ('  ') if designated
+
+start_with? - returns true if string begins with the prefix given ('anti', 'pro')
+
+
+Array
+
+first - returns the first element, or the designated (n) element
+
+delete_at - deletes the given element at the designated index
+
+delete - deletes all items from the array that are equal the designated object
+
+pop - removes the last element from the hash and returns it
+
+Hash
+
+to_a
+
+has_key?
+
+has_value?
+
+
+Time
+
+now
+
+File
+
+
+exist?
+
+extname
